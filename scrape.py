fe60a0ffc8da41b16eac81f0d6a7ac6607693c5d
Tim Porbadnigk <tim.porbadnigk@gmx.de>
2025-06-02 17:42:06 +0200

    implemented simple arguments

--- scrape.py
+++ scrape.py
@@ -1,7 +1,30 @@
 from blackboard import Blackboard
 
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('command')
+parser.add_argument('-m', '--markdown', help='Save the blackboard to FILE', metavar='FILE', type=str)
+
+args = parser.parse_args()
+
+def main() -> None:
+    if args.command in ('blackboard', 'black', 'bl', 'b'):
+        blackboard = Blackboard('https://informatik-mathematik.oth-regensburg.de/schwarzes-brett')
+        blackboard.scrape()
+
+        if args.markdown != None:
+            with open(args.markdown, 'w') as f:
+                f.write(blackboard.to_markdown_str())
+        else:
+            print(blackboard)
+    else:
+        parser.exit(1,parser.format_help())
+
 if __name__ == "__main__":
-    url = 'https://informatik-mathematik.oth-regensburg.de/schwarzes-brett'
-    blackboard = Blackboard(url)
-    blackboard.scrape()
-    print(blackboard)
+    main()
+    #url = 'https://informatik-mathematik.oth-regensburg.de/schwarzes-brett'
+    #blackboard = Blackboard(url)
+    #blackboard.scrape()
+    #print(blackboard)

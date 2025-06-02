4ae67877b593423cd0baef1b612880297dd32a15
Tim Porbadnigk <tim.porbadnigk@gmx.de>
2025-06-02 15:50:29 +0200

    refactored blackboard scraping

--- scrape.py
+++ scrape.py
@@ -1,70 +1,96 @@
 import requests
 from bs4 import BeautifulSoup
 
-
-def scrape_more_links(url: str) -> str:
+class BlackbordEntry:
     """
-    Scrapes the "more" url from the OTH Regensburg blackboard page and returns the text.
-    Does a little markdown formatting for the time.
-    Args:
-        url: The URL of the "more" link to scrape.
-    Returns:
-        The text content of the page at the given URL.
+    Represents an entry on the blackboard and handles
+    converting it to string and markdown
     """
-    res = requests.get(url)
-    res.raise_for_status()  # Check for HTTP errors
-    soup = BeautifulSoup(res.text, 'html.parser')
+    def __init__(self, title: str, published_at: str, content: str):
+        self.title = title
+        self.published_at = published_at
+        self.content = content
 
-    # find the time of creation
-    time=soup.find('time', itemprop='datePublished')
-    # Find the content in the page
-    content = soup.find('span', itemprop='description')
+    def __str__(self) -> str:
+        return f'{self.title}\n{self.published_at} {self.content}'
 
+    def to_markdown(self) -> str:
+        return f'## {self.title}\n**{self.published_at}** {self.content}'
 
-    if content:
-        return f'**{time.get_text(strip=True)}** {content.get_text(strip=True)}' # type: ignore
-    else:
-        raise ValueError(f"No content found at {url}. The structure of the page may have changed.")
-
-def scrape_oth_blackboard(url: str) -> list[tuple[str, str]]:
+class Blackboard:
     """
-    Scrape the OTH Regensburg blackboard for entries.
-    Args:
-        url: The URL of the OTH Regensburg blackboard.
-    Returns:
-        A list of tuples containing the header and teaser text of each entry.
+    Responsible for scraping the OTH IM-Faculty blackboard
+    and converting it's data to strings or markdown format.
     """
-    # Note: The User-Agent header is commented out, as it may not be necessary.
-    #headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
-    response = requests.get(url)
-    response.raise_for_status()  # Check for HTTP errors
-    soup = BeautifulSoup(response.text, 'html.parser')
-    entries = soup.find_all('div', class_='header')
-    more_links = soup.find_all('a', class_='more')
-    
-    result = []
-    for header, more_url in zip(entries, more_links, strict=True):
-        header_text = header.get_text(strip=True)
+    def __init__(self, url: str):
+        self.url = url
+        self.entries: list[BlackbordEntry] = []
 
-        complete_url = url[:url.rfind('/')] + more_url['href']  # type: ignore
+    def scrape(self) -> None:
+        """
+        Scrape the OTH Regensburg blackboard for entries.
+        Args:
+            url: The URL of the OTH Regensburg blackboard.
+        Returns:
+            A list of tuples containing the header and teaser text of each entry.
+        """
+        # Note: The User-Agent header is commented out, as it may not be necessary.
+        #headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+        response = requests.get(self.url)
+        response.raise_for_status()  # Check for HTTP errors
+        soup = BeautifulSoup(response.text, 'html.parser')
+        entries = soup.find_all('div', class_='header')
+        more_links = soup.find_all('a', class_='more')
 
-        content_text = scrape_more_links(complete_url)
+        for header, more_url in zip(entries, more_links, strict=True):
+            header_text = header.get_text(strip=True)
 
-        result.append((header_text, content_text))
-    return result
+            complete_url = url[:url.rfind('/')] + more_url['href']  # type: ignore
 
-def save_to_markdown(entries: list[tuple[str, str]], filename: str):
-    """
-    Save the scraped entries to a markdown file.
-    Args:
-        entries: The list of entries to save.
-        filename: The name of the markdown file.
-    """
-    with open(filename, 'w', encoding='utf-8') as f:
-        for header, teaser in entries:
-            f.write(f"## {header}\n\n{teaser}\n\n")
+            creation_date, content = self.scrape_contents(complete_url)
+
+            self.entries.append(BlackbordEntry(header_text, creation_date, content))
+
+    def scrape_contents(self, url: str) -> tuple[str, str]:
+        """
+        Scrapes the "more" url from the OTH Regensburg blackboard page and returns the text.
+        Does a little markdown formatting for the time.
+        Args:
+            url: The URL of the "more" link to scrape.
+        Returns:
+            a tuple of the creation time and content
+        """
+        res = requests.get(url)
+        res.raise_for_status()  # Check for HTTP errors
+        soup = BeautifulSoup(res.text, 'html.parser')
+
+        # find the time of creation
+        time=soup.find('time', itemprop='datePublished')
+        # Find the content in the page
+        content = soup.find('span', itemprop='description')
+
+
+        if content:
+            return time.get_text(strip=True), content.get_text(strip=True) # type: ignore
+        else:
+            raise ValueError(f"No content found at {url}. The structure of the page may have changed.")
+
+    def __str__(self) -> str:
+        res = ''
+        for entry in self.entries:
+            res += f'{entry}\n'
+        return res
+
+    def to_markdown_str(self) -> str:
+        """
+        Converts the blackboard to a markdown string
+        """
+        res = ''
+        for entry in self.entries:
+            res += entry.to_markdown() + '\n'
+        return res
 
 if __name__ == "__main__":
     url = 'https://informatik-mathematik.oth-regensburg.de/schwarzes-brett'
-    entries = scrape_oth_blackboard(url)
-    save_to_markdown(entries, 'oth_blackboard.md')
+    blackboard = Blackboard(url)
+    blackboard.scrape()
